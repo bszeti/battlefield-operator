e78ca3dfa82800f2d62a47d7670065072c744972
bszeti <bszeti@redhat.com>
2019-09-13 10:11:53 -0400

    fic

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,8 @@
-#Deploy operator in the current namespace
-oc create -f crds/rhte_v1alpha1_battlefield_crd.yaml
+# Add CRD to the cluster - if it's not there yet. 
+#oc get crd battlefields.rhte.demojam.battlefield
+#oc create -f crds/rhte_v1alpha1_battlefield_crd.yaml
 
+#Deploy operator in the current namespace
 oc create -f service_account.yaml
 oc create -f role.yaml
 oc create -f role_binding.yaml
